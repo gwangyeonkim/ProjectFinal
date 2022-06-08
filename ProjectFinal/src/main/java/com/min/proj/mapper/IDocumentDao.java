e8831c28a3bb09004cf6ade547d930aa7101d35d
ABChangHun <xkawj@lch.edu.com>
2022-06-08 17:22:35 +0900

    proj 부분 Service 작업 및 Ctrl 작업진행중. 화면에서 처리는 아직 안하는중입니다. POSTMAN으로 서버 중심으로 개발중 화면은 추후에 한번에 할 예정.

--- ProjectFinal/src/main/java/com/min/proj/mapper/IDocumentDao.java
+++ ProjectFinal/src/main/java/com/min/proj/mapper/IDocumentDao.java
@@ -4,7 +4,7 @@ public interface IDocumentDao {
 
 	/**
 	 * 프로젝트가 생성되면서 문서번호를 할당하는 메소드
-	 * @param projName
+	 * @param projName 프로젝트 이름
 	 * @return 1= 성공
 	 */
 	public int newDoc(String projName);
