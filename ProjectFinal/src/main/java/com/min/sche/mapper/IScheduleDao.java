1a674479880a48eea8d8b6f658b4c06b7c735514
rlarbcjf <rlarbcjf115@naver.com>
2022-06-15 17:45:21 +0900

    header.jsp, projectMain.css 파일 수정(크기 줄임) 완료, 미완료 일정 목록 출력하는 쿼리 statement, mapper, service 추가 메인 페이지 수정 scheboard.jsp

--- ProjectFinal/src/main/java/com/min/sche/mapper/IScheduleDao.java
+++ ProjectFinal/src/main/java/com/min/sche/mapper/IScheduleDao.java
@@ -6,14 +6,21 @@ import java.util.Map;
 import com.min.sche.vo.ScheduleVo;
 import com.min.sche.vo.WbsViewVo;
 
-
 public interface IScheduleDao {
 
 	public List<WbsViewVo> getTeamSchedule(List<String> userList);
+
 	
 	public List<ScheduleVo> pScheduleShow(String mId);
+
 	public int pScheduleInsert(Map<String, Object> map);
+
 	public int pScheduleUpdate(Map<String, Object> map);
+
 	public int pScheduleDelete(Map<String, Object> map);
+
 	
+	public List<WbsViewVo> callIncomplete(String projName);
+
+	public List<WbsViewVo> callComplete(String projName);
 }
